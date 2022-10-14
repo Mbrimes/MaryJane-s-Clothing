76aa1dba7d7b1a38b54fbb9f005b45d503f06189
Marabs <Marho.jak@gmail.com>
2022-10-14 12:33:20 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-class Red extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			brand: "Ford",
-			model: "Mustang",
-			color: "Red",
-			year: 2001
-		};
-	}
-	render() {
-		return(
-		<div>
-			<h1>My {this.state.brand}</h1>
-			<h2>
-			it is a {this.state.color} {this.state.model} from {this.state.year}.
-			</h2>
-		</div>
-		);
-	}
-}
-const ross=ReactDOM.createRoot(document.getElementById('zxx'));
-ross.render(<Red/>);
+import App from './App';
+ 
